74361cabfd244e760d298fe503972ebc22335266
root <ledinhanhtan.stack@gmail.com>
2023-03-06 23:04:31 +0700

    feat: adding top_nav

--- src/components/layout/TopNav.tsx
+++ src/components/layout/TopNav.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
+import { Button } from 'antd'
+import { ClockCircleOutlined } from '@ant-design/icons'
+
 interface TopNavProps {}
 
 const TopNav: React.FC<TopNavProps> = (props) => {
   return (
     <div className="p-top_nav">
-      <div className="p-top_nav__sidebar">clock</div>
+      <div className="p-top_nav__sidebar">
+        <Button
+          icon={<ClockCircleOutlined />}
+          type="text"
+          className="p-top_nav__button p-top_nav__history-menu"
+        />
+      </div>
       <div className="p-top_nav__search__container">Search</div>
       <div className="p-top_nav__right">
         <div>tooltip</div>
