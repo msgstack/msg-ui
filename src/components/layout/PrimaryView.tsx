fff11d7985c473caf7c4907abf89822380103c8e
prd-tan-le <tan.le@paradox.ai>
2023-03-07 19:21:36 +0700

    feat: primary_view layout

--- src/components/layout/PrimaryView.tsx
+++ src/components/layout/PrimaryView.tsx
@@ -3,7 +3,19 @@ import React from 'react'
 interface PrimaryViewProps {}
 
 const PrimaryView: React.FC<PrimaryViewProps> = (props) => {
-  return <div className="p-workspace__primary__view"></div>
+  return (
+    <div className="p-workspace__primary_view">
+      <div className="p-workspace__primary_view_contents">
+        <div className="p-view_header"></div>
+        <div className="p-file_drag_drop__container">
+          <div role="presentation" style={{ display: 'contents' }}>
+            <div className="p-workspace__primary_view_body"></div>
+            <div className="p-workspace__primary_view_footer"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default PrimaryView
