6dbdb891427dabf364fd2efe8ab6223fc9305a86
prd-tan-le <tan.le@paradox.ai>
2023-03-08 21:53:24 +0700

    feat: composer base

--- src/components/input/composer/ComposerActions.tsx
+++ src/components/input/composer/ComposerActions.tsx
@@ -1,9 +1,29 @@
+import { SendOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
 import React from 'react'
 
 interface ComposerActionsProps {}
 
 const ComposerActions: React.FC<ComposerActionsProps> = (props) => {
-  return <div className="c-wysiwyg_container__actions">ComposerActions</div>
+  const onSendMessage = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div className="c-wysiwyg_container__actions">
+      <div className="c-wysiwyg_container__prefix">prefix</div>
+      <div className="c-wysiwyg_container__toolbar">toolbar</div>
+      <div className="c-wysiwyg_container__suffix">
+        <div className="c-wysiwyg_container__send_button--with_options">
+          <Button
+            icon={<SendOutlined />}
+            className="c-wysiwyg_container__button c-wysiwyg_container__button--send"
+            onClick={onSendMessage}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ComposerActions
--- src/components/input/composer/ComposerInput.tsx
+++ src/components/input/composer/ComposerInput.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import classNames from 'classnames'
+import React, { useState } from 'react'
+
+import { MentionsInput, Mention, OnChangeHandlerFunc } from 'react-mentions'
 
 interface ComposerInputProps {}
 
 const ComposerInput: React.FC<ComposerInputProps> = (props) => {
-  return <div className="c-texty_input__container">ComposerInput</div>
+  const users = [
+    { id: '1', display: 'John Smith' },
+    { id: '2', display: 'Jane Doe' },
+  ]
+
+  const [value, setValue] = useState('')
+
+  const onChangeInput: OnChangeHandlerFunc = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  const handleSetValue = (newValue: string) => {
+    console.log(newValue)
+    setValue('')
+  }
+
+  const onKeydownInput = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      handleSetValue(value)
+
+      return
+    }
+  }
+
+  return (
+    <div className="c-texty_input__container">
+      <div className="c-texty_input">
+        <MentionsInput
+          value={value}
+          className={classNames('ql-editor', { ['ql-blank']: !value })}
+          onChange={onChangeInput}
+          onKeyDown={onKeydownInput}
+        >
+          <Mention trigger="@" data={users} />
+        </MentionsInput>
+        <div className="ql-clipboard" />
+        <div className="ql-placeholder">
+          <div className="c-texty_input__placeholder">Message _kong</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ComposerInput
